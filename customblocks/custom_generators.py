32e110eb111d5327dbef70630d70d98005db1dc4
Danijela Popović <vlajna95@gmail.com>
2022-02-02 18:36:33 +0100

    Adapted the question generator for my Django project :)

--- customblocks/custom_generators.py
+++ customblocks/custom_generators.py
@@ -2,7 +2,7 @@ from random import shuffle
 from .utils import E, slugify
 from .utils import Markdown as M
 
-def question(ctx, correct, answers, unique_name, *args, **kwargs):
+def question(ctx, correct, answers, unique_name, ok_button_text="OK", correct_answer_message="Bravo!", wrong_answer_message="Hmmm... no.", *args, **kwargs):
 	question_name = slugify(unique_name)
 	options = answers.split("|")
 	shuffle(options)
@@ -11,15 +11,17 @@ def question(ctx, correct, answers, unique_name, *args, **kwargs):
 		answer_id = slugify(i)[::-1]
 		if i == correct:
 			answer_list.insert(0, E("input", type="hidden", id=question_name+"_correct", name=question_name+"_correct", value=answer_id))
-		answer = E("input", type="radio", id=answer_id, name=question_name)
+		answer = E("input", type="radio", id=answer_id, name=question_name, value=i)
 		answer_label = E("label", i)
 		answer_label.set("for", answer_id)
 		br = E("br")
 		answer_list.append(answer)
 		answer_list.append(answer_label)
 		answer_list.append(br)
-	ok_button = E("button.ok", "Odgovori", type="button", onclick="checkAnswer(this)")
+	ok_button = E("button.ok", ok_button_text, type="button", onclick="checkAnswer(this, '"+correct_answer_message+"', '"+wrong_answer_message+"')")
 	ok_button.set("data-question", question_name)
 	ok_button.set("data-correct", question_name+"_correct")
-	question = E("."+ctx.type, dict(_class=" ".join(["-".join(arg.split()) for arg in args])), M(ctx.content, ctx.parser), answer_list, ok_button, **kwargs)
+	question = E("."+ctx.type, dict(_class=" ".join(["-".join(arg.split()) for arg in args])), M(ctx.content, ctx.parser), **kwargs)
+	question.append(answer_list)
+	question.append(ok_button)
 	return question
